4be8c3be05834bc4771d8db8393714d37600d355
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-11-15 13:57:02 +0000

    レイアウト

--- app/models/sauna.rb
+++ app/models/sauna.rb
@@ -5,12 +5,12 @@ class Sauna < ApplicationRecord
  has_many :favorites, dependent: :destroy
  has_many :comments, dependent: :destroy
 
-  def get_profile_image
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 
