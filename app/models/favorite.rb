aa986ce35923bf2cc3fdbaab6148110f508695ba
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-11-23 11:39:05 +0000

    変更

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,5 @@
 class Favorite < ApplicationRecord
 	 belongs_to :user
 	 belongs_to :sauna
-	 validates_uniqueness_of :book_id, scope: :user_id
+	 validates_uniqueness_of :sauna_id, scope: :user_id
 end
