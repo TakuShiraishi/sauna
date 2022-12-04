c3b1305218c27d05d19d9ca366f4da5ce2c032d4
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-12-04 12:51:47 +0000

    test

--- app/controllers/public/saunas_controller.rb
+++ app/controllers/public/saunas_controller.rb
@@ -24,6 +24,7 @@ class Public::SaunasController < ApplicationController
 
 
  def create
+  byebug
   @sauna = Sauna.new(sauna_params)
    @sauna.score = Language.get_data(sauna_params[:comment])  #この行を追加
   @sauna.user_id = current_user.id
